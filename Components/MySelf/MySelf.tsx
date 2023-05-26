17c40507840f5ad93cc86382cb74a2350bcff46d
Abhinav TV <abhinavgit8@gmail.com>
2023-05-26 15:16:20 +0530

    somr changes are there

--- Components/MySelf/MySelf.tsx
+++ Components/MySelf/MySelf.tsx
@@ -11,9 +11,17 @@ function MySelf(props: string) {
       <DetailPage
 
         heading="FIRST DAY TO A NEW BEGINNING OF LIFE."
-        description= ''
 
-
+        description="As we have all heard 'every new beginning comes from some other beginning's end'. As a girl who always wished to do her graduation far away from her home and also being an only daughter at home it was really difficult for me to take such a decision. Any how as everyone could imagine being an only daughter and leaving my 
+        parents away and migrating to such a city as Bangalore was a really challenging task for me. Applying for CBZ course in Christ University was not a thing that I had in my mind. Never bad I thought
+         about it also. It was my mom who had send my application form for Christ Deemed to be University. 
+        She told me about this only when my interview date was send to my email. Never ever in my dreams had I thought about learning in such a big city like Bangalore. All that had passed and I had my interview. My interview was not that easy and I had a really tough time attending it, though the teachers were sweet. The questions were really hard. 
+        I had no hope of getting through my interview at Christ, but surprisingly I received an email after a week from Christ University.
+        I was really happy but my mom and dad was not that happy cause they would have to leave me alone here at Bangalore and they could never imagine of such a thing happening. We had very short time to plan and prepare to come to Bangalore cause my class began at 10th of June. 9 would never forget the first day in campus.
+         1 could see many gloomy faces many charming enthusiastic and also very happy faces were some are sad about leaving their parents some bappy to Join a new place and all sorts of things happening. 
+        1 had only Just one thought in my mind and that was Just about leaving mom and dad. After all the cultural programmes it was time for us to go to our classes and to understand the environment. I could still remember the fully filled eyes of my mother who never wanted to leave me alone as any other mother would think.
+         And i could also see my father wiping off his tears making sure 
+        nobody is watching him. Reaching the class after that the first day i thought i could never make friends with any of them here. But by time I got many friends here at Christ University and it has been really good time being her"
       />
 
 
--- Components/UI/DetailPage/DetailPage.tsx
+++ Components/UI/DetailPage/DetailPage.tsx
@@ -4,21 +4,23 @@ import { H3, H5 } from '../Typography'
 
 export default function DetailPage(props: any) {
 
-    const { heading,description } = props
+    const { heading, description } = props
 
     return (
         <Grid container justifyContent="center">
 
-            <Grid container  sx={{
+            <Grid container sx={{
                 py: { xs: 2, sm: 3, md: 4, lg: 4 }
             }}>
                 <H3 textAlign="center" fontWeight="bold" color="#A9A4DD">{heading}</H3>
             </Grid>
 
-            <Grid container lg={10}  >
-                <H5 textAlign="center">{description}</H5>
+            <Grid sx={{ textAlign: 'justify' }} container xs={10} sm={10} md={10} lg={10}   >
+                <H5 fontWeight="550">{description}</H5>
             </Grid>
 
-        </Grid>
+
+
+        </Grid >
     )
 }
