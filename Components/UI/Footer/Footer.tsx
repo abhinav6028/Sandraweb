a52da5fe4f422207248bd93d4cfff04099e50363
Abhinav TV <abhinavgit8@gmail.com>
2023-06-01 17:14:47 +0530

    changes are applied

--- Components/UI/Footer/Footer.tsx
+++ Components/UI/Footer/Footer.tsx
@@ -1,24 +1,46 @@
-import { Box, Grid } from '@mui/material'
-import React from 'react'
+import { Box, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import useSound from 'use-sound';
+import Sound from '../Sound/Sound';
+//import boopSfx from '../../../public/Assets/Sounds/bhoom.mp3';
+
 
 export default function Footer() {
 
-    const playSound = () => {
+    const URL = '../../../public/Assets/Sounds/bhoom.mp3';
 
-        alert('jjjjjjjjjjjjjjjjjjjjjjjjj')
-        
-    }
+    const [sound, setSound] = useState(false)
+
 
     return (
-        <Grid container justifyContent="center" sx={{ mt: 4 }}>
 
-            <Box onClick={playSound}>
 
-                Footer
+
+        < Grid container justifyContent="center" sx={{ mt: 4 }}>
+
+            {sound ? <Sound  /> : null}
+
+            <Box onClick={() => setSound(!sound)}>
+
+                <Box
+                    component="img"
+                    sx={{
+                        height: 233,
+                        width: 350,
+                        maxHeight: { xs: 233, md: 167 },
+                        maxWidth: { xs: 350, md: 250 },
+                    }}
+                    alt="The house from the offer."
+                    src="/"
+                />
+
+                {/* <Sound /> */}
+                
 
             </Box>
 
+        </Grid >
 
-        </Grid>
     )
+
 }
